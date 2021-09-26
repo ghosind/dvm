8d81bf35b14d7a12b18a534ff59738febb938d70
ghosind <ghosind@gmail.com>
2021-09-26 23:52:18 +0800

    Check deno file after uninstall

--- test/test_uninstall_version.sh
+++ test/test_uninstall_version.sh
@@ -13,16 +13,19 @@ dvm current | grep "v1.14.0" || dvm_error "[ERR] current version should be v1.14
 
 # Uninstall by current
 dvm uninstall current || dvm_error "[ERR] 'dvm uninstall current' failed"
+[ -f "$DVM_DIR/versions/v1.14.0/deno" ] && dvm_error "[ERR] deno v1.14.0 should be uninstalled"
 
-# Install deno again and set alias name 'default' to it
+# Install deno v1.14.0 again and set alias name 'default' to it
 dvm install v1.14.0
 dvm alias default v1.14.0 || dvm_error "[ERR] 'dvm alias default v1.14.0' failed"
 
 # Uninstall by alias name
 dvm uninstall default || dvm_error "[ERR] 'dvm uninstall default' failed"
+[ -f "$DVM_DIR/versions/v1.14.0/deno" ] && dvm_error "[ERR] deno v1.14.0 should be uninstalled"
 
-# Install deno again
+# Install deno v1.14.0 again
 dvm install v1.14.0
 
 # Uninstall by version
 dvm uninstall v1.14.0 || dvm_error "[ERR] 'dvm uninstall v1.14.0' failed"
+[ -f "$DVM_DIR/versions/v1.14.0/deno" ] && dvm_error "[ERR] deno v1.14.0 should be uninstalled"
