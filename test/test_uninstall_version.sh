a9ad660cdae9a643fb7ce9af63f6a2afe9516e8a
Chen Su <ghosind@gmail.com>
2021-09-28 17:40:51 +0800

    test: remove uninstall by current.

--- test/test_uninstall_version.sh
+++ test/test_uninstall_version.sh
@@ -8,16 +8,7 @@ dvm_error() {
 # shellcheck disable=SC1091
 \. ./dvm.sh
 
-# Set and check current version
-dvm use v1.14.0 || dvm_error "[ERR] 'dvm use v1.14.0' failed"
-dvm current | grep "v1.14.0" || dvm_error "[ERR] current version should be v1.14.0"
-
-# Uninstall by current
-dvm uninstall current || dvm_error "[ERR] 'dvm uninstall current' failed"
-[ -f "$DVM_DIR/versions/v1.14.0/deno" ] && dvm_error "[ERR] deno v1.14.0 should be uninstalled"
-
-# Install deno v1.14.0 again and set alias name 'default' to it
-dvm install v1.14.0
+# Set alias name 'default' to v1.14.0
 dvm alias default v1.14.0 || dvm_error "[ERR] 'dvm alias default v1.14.0' failed"
 
 # Uninstall by alias name
