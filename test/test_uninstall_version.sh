dafc5f477a59b33ce0b79ef9bda6a9d0f67c8dc7
ghosind <ghosind@gmail.com>
2021-09-29 23:02:56 +0800

    Fix uninstall test error

--- test/test_uninstall_version.sh
+++ test/test_uninstall_version.sh
@@ -13,11 +13,11 @@ dvm alias default v1.14.0 || dvm_error "[ERR] 'dvm alias default v1.14.0' failed
 
 # Uninstall by alias name
 dvm uninstall default || dvm_error "[ERR] 'dvm uninstall default' failed"
-[ -f "$DVM_DIR/versions/v1.14.0/deno" ] && dvm_error "[ERR] deno v1.14.0 should be uninstalled"
+[ ! -f "$DVM_DIR/versions/v1.14.0/deno" ] || dvm_error "[ERR] deno v1.14.0 should be uninstalled"
 
 # Install deno v1.14.0 again
 dvm install v1.14.0
 
 # Uninstall by version
 dvm uninstall v1.14.0 || dvm_error "[ERR] 'dvm uninstall v1.14.0' failed"
-[ -f "$DVM_DIR/versions/v1.14.0/deno" ] && dvm_error "[ERR] deno v1.14.0 should be uninstalled"
+[ ! -f "$DVM_DIR/versions/v1.14.0/deno" ] || dvm_error "[ERR] deno v1.14.0 should be uninstalled"
