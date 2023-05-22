7dba119e2c5cbfb9363152c65d579b4180913e1e
ghosind <ghosind@gmail.com>
2023-05-22 22:10:10 +0800

    Add test case for ls-remote command.

---
+++ test/test_ls_remote.sh
@@ -0,0 +1,14 @@
+dvm_test_error() {
+  echo "[ERR]" "$@"
+  exit 1
+}
+
+# shellcheck disable=SC1091
+\. ./dvm.sh || dvm_test_error "failed to install dvm"
+
+versions=$(dvm ls-remote)
+
+[ "$(echo $versions | grep v0.1.0)" = "" ] && dvm_test_error "Deno v0.1.0 should in the remote versions list"
+
+# Not same page
+[ "$(echo $versions | grep v1.33.0)" = "" ] && dvm_test_error "Deno v1.33.0 should in the remote versions list"
