2d7062cc997a870500a85c038c4e987c3e7cf864
ghosind <ghosind@gmail.com>
2023-05-22 22:33:03 +0800

    Fix ls-remote test case error.

--- test/test_ls_remote.sh
+++ test/test_ls_remote.sh
@@ -10,7 +10,7 @@ dvm_test_error() {
 
 versions=$(dvm ls-remote)
 
-[ "$(echo "$versions" | grep v0.1.0)" = "" ] || dvm_test_error "Deno v0.1.0 should in the remote versions list"
+[ "$(echo "$versions" | grep v0.1.0)" != "" ] || dvm_test_error "Deno v0.1.0 should in the remote versions list"
 
 # Not same page
-[ "$(echo "$versions" | grep v1.33.0)" = "" ] || dvm_test_error "Deno v1.33.0 should in the remote versions list"
+[ "$(echo "$versions" | grep v1.33.0)" != "" ] || dvm_test_error "Deno v1.33.0 should in the remote versions list"
