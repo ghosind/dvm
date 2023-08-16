ea31eac8ba65f101f8062c2838ee903df9b07031
ghosind <ghosind@gmail.com>
2023-08-16 20:11:46 +0800

    Add test case of installing Deno from source code.

---
+++ test/test_install_from_source.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dvm_test_error() {
+  echo "[ERR]" "$@"
+  exit 1
+}
+
+# shellcheck disable=SC1091
+\. ./dvm.sh || dvm_test_error "failed to install dvm"
+
+# Install deno v1.36.0
+TARGET_VERSION="1.36.0"
+dvm install "v$TARGET_VERSION" --from-source --verbose || dvm_test_error "run 'dvm install v$TARGET_VERSION' failed"
+
+# Check installed version directory
+[ -d "$DVM_DIR/versions/v$TARGET_VERSION" ] || dvm_test_error "'$DVM_DIR/versions/v$TARGET_VERSION' is not a directory"
+
+# Check deno version
+dvm run "v$TARGET_VERSION" --version | grep "deno $TARGET_VERSION" || dvm_test_error "deno is invalid"
+
+# Set active version
+dvm use "v$TARGET_VERSION" --verbose || dvm_test_error "run 'dvm use v$TARGET_VERSION' failed"
+
+# Check with ls command
+dvm ls | grep "\-> v$TARGET_VERSION" || dvm_test_error "run 'dvm ls' failed"
