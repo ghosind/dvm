74bf5df93c2a3e0b492e3a682a2e8981740547f6
Chen Su <ghosind@gmail.com>
2024-07-29 14:02:28 +0800

    Fix test case error.
    Signed-off-by: Chen Su <ghosind@gmail.com>

--- test/test_install_version_by_prefix.sh
+++ test/test_install_version_by_prefix.sh
@@ -5,12 +5,18 @@ dvm_test_error() {
   exit 1
 }
 
+# shellcheck disable=SC1091
+\. ./dvm.sh || dvm_test_error "failed to install dvm"
+
 # Install deno by prefix
 dvm install 1.44 || dvm_test_error "run 'dvm install 1.44' failed"
 dvm ls | grep "v1.44.4" || dvm_test_error "run 'dvm ls' failed"
 
-if [ "$(uname -s)" = "Linux" ]
+# Skip if run on MacOS with m-chip
+if [ "$(uname -s)" = "Drawin" ] && [ "$(uname -m)" = "arm64" ]
 then
-  dvm install 1.2 || dvm_test_error "run 'dvm install 1.2' failed"
-  dvm ls | grep "v1.2.3" || dvm_test_error "run 'dvm ls' failed"
+  exit 0
 fi
+
+dvm install 1.2 || dvm_test_error "run 'dvm install 1.2' failed"
+dvm ls | grep "v1.2.3" || dvm_test_error "run 'dvm ls' failed"
