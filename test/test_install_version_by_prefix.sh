858c5961e5d162bf4b8325283ad2663f3c286819
ghosind <ghosind@gmail.com>
2024-07-30 22:09:59 +0800

    Fix prefix test case error on macOS with m-chip.
    Signed-off-by: ghosind <ghosind@gmail.com>

--- test/test_install_version_by_prefix.sh
+++ test/test_install_version_by_prefix.sh
@@ -9,14 +9,14 @@ dvm_test_error() {
 \. ./dvm.sh || dvm_test_error "failed to install dvm"
 
 # Install deno by prefix
-dvm install 1.44 || dvm_test_error "run 'dvm install 1.44' failed"
+dvm install 1.44  --verbose || dvm_test_error "run 'dvm install 1.44' failed"
 dvm ls | grep "v1.44.4" || dvm_test_error "run 'dvm ls' failed"
 
 # Skip if run on MacOS with m-chip
-if [ "$(uname -s)" = "Drawin" ] && [ "$(uname -m)" = "arm64" ]
+if [ "$(uname -s)" = "Darwin" ] && [ "$(uname -m)" = "arm64" ]
 then
   exit 0
 fi
 
-dvm install 1.2 || dvm_test_error "run 'dvm install 1.2' failed"
+dvm install 1.2  --verbose || dvm_test_error "run 'dvm install 1.2' failed"
 dvm ls | grep "v1.2.3" || dvm_test_error "run 'dvm ls' failed"
