d24f3af7f2f239dfd8d9789742b602e15904073b
ghosind <ghosind@gmail.com>
2024-09-21 16:13:46 +0800

    Do not use verbose mode for tests to hide api token.
    Signed-off-by: ghosind <ghosind@gmail.com>

--- test/test_install_version.sh
+++ test/test_install_version.sh
@@ -10,25 +10,25 @@ dvm_test_error() {
 
 # Install deno v1.0.0
 TARGET_VERSION="1.45.0"
-dvm install "v$TARGET_VERSION" --skip-validation --verbose || dvm_test_error "run 'dvm install v$TARGET_VERSION' failed"
+dvm install "v$TARGET_VERSION" --skip-validation || dvm_test_error "run 'dvm install v$TARGET_VERSION' failed"
 
 # Check installed version directory
 [ -d "$DVM_DIR/versions/v$TARGET_VERSION" ] || dvm_test_error "'$DVM_DIR/versions/v$TARGET_VERSION' is not a directory"
 
 # Install latest version
-dvm install --verbose --skip-validation
+dvm install --skip-validation
 
 # Install another deno
-dvm install v1.40.0 --verbose --skip-validation
+dvm install v1.40.0 --skip-validation
 
 # Install deno by shortcut
-dvm i v1.45.0 --verbose --skip-validation
+dvm i v1.45.0 --skip-validation
 
 # Check deno version
 dvm run "v$TARGET_VERSION" --version | grep "deno $TARGET_VERSION" || dvm_test_error "deno is invalid"
 
 # Set active version
-dvm use "v$TARGET_VERSION" --verbose || dvm_test_error "run 'dvm use v$TARGET_VERSION' failed"
+dvm use "v$TARGET_VERSION" || dvm_test_error "run 'dvm use v$TARGET_VERSION' failed"
 
 # Check with ls command
 dvm ls | grep "\-> v$TARGET_VERSION" || dvm_test_error "run 'dvm ls' failed"
